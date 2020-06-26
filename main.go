cc3b82687f85ca5c28b49f207348cfc95dc2b2b1
wenxinxiang <wenxinxiang@tencent.com>
2020-06-26 16:06:19 +0800

    register grpc server

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/NothingXiang/go-rpc/api"
+	"github.com/NothingXiang/go-rpc/server"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	listener, err := net.Listen("tcp", ":2345")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	grpcServer := grpc.NewServer(nil)
+
+	api.RegisterMyServiceServer(grpcServer, &server.Server{})
+
+	grpcServer.Serve(listener)
 
 }
--- server/server.go
+++ server/server.go
@@ -1,2 +1,14 @@
 package server
 
+import (
+	context "context"
+
+	"github.com/NothingXiang/go-rpc/api"
+)
+
+type Server struct {
+}
+
+func (s *Server) Echo(ctx context.Context, message *api.StringMessage) (*api.Person, error) {
+	panic("implement me")
+}
